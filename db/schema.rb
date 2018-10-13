11f5cc111b2b004ba49a74744ab8d9820d3cc558
tagkuya <fortis931taku@gmail.com>
2018-10-13 23:02:36 +0900

    ububtu

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_08_082113) do
+ActiveRecord::Schema.define(version: 2018_10_13_084542) do
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
