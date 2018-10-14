c0aa815ab85e1231b474a64d80850f98e3ecaac9
tagkuya <fortis931taku@gmail.com>
2018-10-14 13:47:56 +0900

    Integated new and create actions for portfolos

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,27 @@
 class PortfoliosController < ApplicationController
     def index   
-        @portfolio_items =Portfolio.all()
+        @portfolio_items =Portfolio.all
     end
+
+    def new 
+        @portfolio_item = Portfolio.new    
+    end
+
+    def create
+        @portfolio_item = Portfolio.new(reauire_parms_portfolio)
+        
+        respond_to do |format|
+            if @portfolio_item.save 
+                format.html{redirect_to portfolios_path, notice: 'Your Post is now live'}
+            else
+                format.html{ render :new }
+            end
+        end
+    end
+
+    private 
+    def reauire_parms_portfolio
+        params.require(:portfolio).permit(:title, :subtitle, :body)
+    end
+
 end
